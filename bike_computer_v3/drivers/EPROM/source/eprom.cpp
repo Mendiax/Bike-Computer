3b3162885f7e45f32ac3724dac76b2abfc2e8dbd
Mendiax <254658@student.pwr.edu.pl>
2022-09-21 09:24:35 +0200

    eprom addr fix

--- bike_computer_v3/drivers/EPROM/source/eprom.cpp
+++ bike_computer_v3/drivers/EPROM/source/eprom.cpp
@@ -14,8 +14,7 @@
 // #------------------------------#
 // |           macros             |
 // #------------------------------#
-#define WRITE_ADDR 10100000
-#define READ_ADDR 10100001
+#define EPROM_ADDR 1010000
 
 
 // #------------------------------#
@@ -36,15 +35,15 @@
 uint8_t eprom::read_byte(uint8_t address)
 {
 	uint8_t TempVal = 0;
-	i2c_write_blocking(i2c1, WRITE_ADDR, &address, 1, true);  // true to keep master control of bus
+	i2c_write_blocking(i2c1, EPROM_ADDR, &address, 1, true);  // true to keep master control of bus
     // read in one go as register addresses auto-increment
-    i2c_read_blocking(i2c1, READ_ADDR, &TempVal, 1, false);  // false, we're done reading
+    i2c_read_blocking(i2c1, EPROM_ADDR, &TempVal, 1, false);  // false, we're done reading
 	return TempVal;
 }
 void eprom::write_byte(uint8_t address, uint8_t byte)
 {
     uint8_t packet[] = {address, byte};
-    i2c_write_blocking(i2c1, WRITE_ADDR, packet, sizeof(packet), false);
+    i2c_write_blocking(i2c1, EPROM_ADDR, packet, sizeof(packet), false);
 }
 
 // #------------------------------#
