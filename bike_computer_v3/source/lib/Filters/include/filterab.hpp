848d4b9acd2e97c83670775f692545f94e739d60
Mendiax <54842997+Mendiax@users.noreply.github.com>
2023-05-13 09:49:13 +0200

    [FEATURE] filters for parsing input (#50)
    * [FEATURE] filters for parsing input
    Description:
    - new low pass filter
    - aplied it for bmp280 pressure
    - plot pressure

---
+++ bike_computer_v3/source/lib/Filters/include/filterab.hpp
@@ -0,0 +1,38 @@
+#ifndef FILTERAB_HPP
+#define FILTERAB_HPP
+// #-------------------------------#
+// |           includes            |
+// #-------------------------------#
+// pico includes
+
+// c/c++ includes
+
+
+// my includes
+
+// #-------------------------------#
+// |            macros             |
+// #-------------------------------#
+
+// #-------------------------------#
+// | global types declarations     |
+// #-------------------------------#
+template<typename T>
+class Filter_Alpha_Beta{
+public:
+
+private:
+
+public:
+    Filter_Alpha_Beta();
+};
+
+// #-------------------------------#
+// | global variables declarations |
+// #-------------------------------#
+
+// #-------------------------------#
+// | global function declarations  |
+// #-------------------------------#
+
+#endif
