6cc212cbb1e5ca15dc7825373ed1c6a88a710110
Mendiax <bdzikowski11@gmail.com>
2023-05-13 17:45:13 +0200

    [FEATURE] FreeRTOS submodule added
    Description:
    - addes submodule FreeRTOS
    - basic test for FreeRTOS

--- bike_computer_v3/tests/hw/target_main_1.cpp
+++ bike_computer_v3/tests/hw/target_main_1.cpp
@@ -2,50 +2,71 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 
+#include "pico/time.h"
 #include "tests/test_cases.h"
 #include "bc_test.h"
 #include "pico_test.hpp"
 #include "traces.h"
 
-#include "interrupts/interrupts.hpp"
-#include "speedometer/speedometer.hpp"
-#include "cadence/cadence.hpp"
+#include <FreeRTOS.h>
+#include <task.h>
 
 
-#define LED_PIN 25
-
-
-int test_basic()
+void led_task(void *pvParameters)
 {
-    BC_CHECK_EQ(1,1);
-    return 0;
+    (void)pvParameters;
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(100));
+        gpio_put(LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(100));
+        printf("led tesk loop\n");
+    }
+}
+
+void stop_all(void *pvParameters)
+{
+    (void)pvParameters;
+    const int delay = *((int*)pvParameters);
+    vTaskDelay(pdMS_TO_TICKS(delay));
+    printf("vTaskEndScheduler()\n");
+    // vTaskEndScheduler();
+
+    // freeze
+    vTaskSuspendAll();
+    pico_test_end();
+    while (true) {
+        sleep_ms(500);
+        printf("blink\n");
+    }
 }
 
 int main()
 {
-    bi_decl(bi_program_description("First Blink"));
-    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+    bi_decl(bi_program_description("FreeRTOS test"));
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, 1);
     printf("main()\n");
 
     stdio_init_all();
     traces_init();
 
-    interruptSetupCore0();
-    interruptSetupCore1();
-
     pico_test_start();
 
-    printf("second test file\n");
+
+    int time_limit = 5000;
+    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    xTaskCreate(stop_all, "STOP_Task", 256, &time_limit, tskIDLE_PRIORITY, NULL);
+    printf("vTaskStartScheduler()\n");
+    vTaskStartScheduler();
+
+
+
 
     pico_test_end();
     while (true) {
-        gpio_put(LED_PIN, 0);
-        sleep_ms(500);
-        gpio_put(LED_PIN, 1);
         sleep_ms(500);
         printf("blink\n");
     }
